116af72bbfba4c47f89fc1116aeda89e70901bc6
ysmohith-07 <ysaimohith07@gmail.com>
2022-07-06 21:24:07 +0530

    acc

---
+++ myaccount.js
@@ -0,0 +1,25 @@
+// let btn1=document.getElementById("detailsBtn1");
+// let details1=document.getElementById("details1");
+
+let arr=['arr1','arr2','arr3','arr4']
+
+function details1(){
+    var tag=document.createElement('p');
+    var text=document.createTextNode(arr[3]);
+    tag.appendChild(text);
+    document.getElementById('details1').classList.remove("invisible");
+    document.getElementById('details2').classList.add("invisible");
+    document.getElementById('details1').appendChild(tag);
+    // btn1.style.color="blue";
+
+}
+
+function details2(){
+    document.getElementById('details2').classList.remove("invisible");
+    document.getElementById('details1').classList.add("invisible");
+    // btn1.style.color="blue";
+}
+
+
+
+console.log("hi");
